d2ae766cf5bf00b54cf5d9aab4b74c7e31578951
deepak <deepak@excellencetechnologies.in>
2020-10-20 14:46:22 +0530

    html and css assignment

---
+++ ass1layout2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Layout1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="ass1layout2.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="navigation"> 
+    <li><a class="navigationlink1" href="#">Layout3</a></li>
+    <li><a class="navigationlink2" href="#">About</a></li>
+    <li><a class="navigationlink3" href="#">Service</a></li>
+    <li><a class="navigationlink4" href="#">Contact</a></li>
+</div>
+
+<div class="maindiv"> 
+    <h2 class="headingstyle"> Sample Heading  </h2>
+    <p class="paragraphstyle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <button type="button" class="buttonstyle"  > Call to ation! </button>
+</div>
+
+</body>
+
+</html>
---
+++ assignment6.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Bootstrap Example</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="assignment6.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+   </head>
+   <body>
+      <div class="maindiv">
+         <!-- header part -->
+         <header>
+            <!-- first nav -->
+           <div class="headerdiv">
+           <nav class="navbar navbar-default">
+               <div class="container-fluid">
+                  <div class="navbar-header">
+                    <img src="ass6logo.png" class="imgstyle"/>
+                  </div>
+                  <ul class="nav navbar-nav">
+                   
+                     <li><a class="nav-link StartwestSitestyle" href="#">Startwest Sites</a></li>
+
+                     <li class="nav-item designlink">
+                  <a class="nav-link stylelink" href="#">Ingredients</a>
+                  <a class="nav-link stylelink" href="#">Retail</a>
+                  <a class="nav-link stylelink" href="#">Wholesale</a>
+               </li>
+               <!-- <li class="nav-item searchstyle">
+                  <input type="search" placeholder ="Search Products" class="inputsearch">
+                  <button type = "button" class="gobuttonstyle">Go</button>
+               </li> -->
+                  </ul>
+               </div>
+            </nav>
+            <!-- second nav -->
+            <!-- <nav class="navbar navbar-default">
+               <div class="container-fluid">
+                  <div class="navbar-header">
+                     <a class="navbar-brand" href="#">WebSiteName</a>
+                  </div>
+                  <ul class="nav navbar-nav">
+                     <li class="active"><a href="#">Home</a></li>
+                     <li><a href="#">Page 1</a></li>
+                     <li><a href="#">Page 2</a></li>
+                     <li><a href="#">Page 3</a></li>
+                  </ul>
+               </div>
+            </nav> -->
+           </div>
+         </header>
+         <!-- section part -->
+         <section>
+         <section>
+      </div>
+   </body>
+</html>
